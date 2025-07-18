751dfa167c2ae845f13c9bc64bf6fd6bb87fd3e7
LAPTOP-KKBR68BP\ASUS <tartar0085@gmail.com>
2025-07-18 17:36:27 +0700

    แก้ config , Build and deploy portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import AboutHeader from "../components/sections/AboutSection";
-import BannerSection from "../components/sections/BannerSection";
-import SkillsSection from "../components/sections/SkillsSection";
-import ProjectsSection from "../components/sections/ProjectSection";
-import ExperienceSection from "../components/sections/ExperienceSection";
-import TimelineSection from "../components/sections/TimelineSection";
-import CertificatesSection from "../components/sections/CertificatesSection";
-import DownloadCVSection from "../components/sections/DownloadCVSection";
-import ContactSection from "../components/sections/ContactSection";
+import AboutSection from '@/components/sections/AboutSection'
+import BannerSection from "@/components/sections/BannerSection";
+import SkillsSection from "@/components/sections/SkillsSection";
+import ProjectsSection from "@/components/sections/ProjectSection";
+import ExperienceSection from "@/components/sections/ExperienceSection";
+import TimelineSection from "@/components/sections/TimelineSection";
+import CertificatesSection from "@/components/sections/CertificatesSection";
+import DownloadCVSection from "@/components/sections/DownloadCVSection";
+import ContactSection from "@/components/sections/ContactSection";
 
 export default function Home() {
   return (
     <>
-      <AboutHeader />
+      <AboutSection />
       <BannerSection />
       <SkillsSection />
       <ProjectsSection />
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
+  output: 'export',
   webpack: (config: { resolve: { alias: { [x: string]: any; }; }; }) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
