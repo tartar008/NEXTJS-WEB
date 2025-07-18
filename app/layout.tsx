683ddcde7351c52529e4c3f789240b2e7b60d236
LAPTOP-KKBR68BP\ASUS <tartar0085@gmail.com>
2025-07-18 16:24:08 +0700

    แก้ layout กับ config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css';
 import { Metadata } from 'next';
 import Script from 'next/script';
-import Head from 'next/head';
-import AosInit from '@/components/AosInit'; // ✅ เพิ่มการ import
+import AosInit from '@/components/AosInit';
 import Navbar from '@/components/navigation';
 
 export const metadata: Metadata = {
@@ -13,8 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        {/* CSS ที่ต้องโหลดจาก CDN */}
+      <head>
         <link
           href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
           rel="stylesheet"
@@ -23,9 +21,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://unpkg.com/aos@2.3.4/dist/aos.css"
           rel="stylesheet"
         />
-      </Head>
+      </head>
       <body style={{ backgroundColor: '#f5f5f5' }}>
-        {/* JS จาก CDN */}
         <Script
           src="https://cdn.jsdelivr.net/npm/gsap@3.11.1/dist/gsap.min.js"
           strategy="beforeInteractive"
